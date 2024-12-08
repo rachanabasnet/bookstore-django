85b62e54d891790bf60f3679452badf616f7560b
Rachana Basnet <rachanabasnet19@gmail.com>
2024-12-08 17:18:40 -0500

    add cart template, view

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from cart.models import Cart, CartItem, Order
+
 # Register your models here.
+admin.site.register([
+    Cart,
+    CartItem,
+    Order
+])
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+from cart.models import CartItem
+
+
 # Create your views here.
 def cart(request):
-    return render(request, 'cart.html')
+    user_cart = request.user.cart.filter(is_active=True).first()
+    if user_cart:
+        items = CartItem.objects.filter(cart=user_cart)
+        cart_data = {
+        'subtotal': user_cart.get_total(),
+        'shipping': 5.99,
+        'total': user_cart.get_total() + 5.99,
+        }
+    else :
+        items = CartItem.objects.none()
+        cart_data = {}
+
+    return render(request, 'cart.html', {'cart': cart_data, 'items': items})
