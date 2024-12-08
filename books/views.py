e09c7092aa405c836eb193ba270e5e388996f5a8
Rachana Basnet <rachanabasnet19@gmail.com>
2024-12-08 17:36:44 -0500

    display cart button if added to cart

--- books/views.py
+++ books/views.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from books.models import Book
+from cart.models import Cart
 
 
 # Create your views here.
@@ -11,6 +11,18 @@ def index(request):
 
 
 def books(request):
-    template_name = 'books.html'
-    book_list = Book.objects.all()
-    return render(request, template_name, {'books': book_list})
+    active_cart = Cart.objects.filter(user=request.user, is_active=True).first()
+    book_list = []
+
+    if active_cart:
+        cart_items = active_cart.items.all()
+        cart_books = {item.book.id: item.quantity for item in cart_items}
+
+
+        for book in Book.objects.all():
+            is_in_cart = cart_books.get(book.id, 0)
+            book_list.append({'item': book, 'is_in_cart': True if is_in_cart > 0 else False})
+    else:
+        for book in Book.objects.all():
+            book_list.append({'item': book, 'is_in_cart': False})
+    return render(request, 'books.html', {'books': book_list})
